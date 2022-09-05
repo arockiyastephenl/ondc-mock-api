847924d06619fb7dfa92cf3d84ae4e9501ab7feb
arockiyastephenl <lourdustephen95@gmail.com>
2022-09-05 12:46:45 +0530

    rabit code diable

--- index.js
+++ index.js
@@ -30,31 +30,31 @@ app.use(cookieParser());
 
 let Received_msg_id = ""
 
-amqp.connect('amqp://140.238.207.118', function (error0, connection) {
-  if (error0) {
-    throw error0;
-  }
-  connection.createChannel(function (error1, channel) {
-    if (error1) {
-      throw error1;
-    }
+// amqp.connect('amqp://140.238.207.118', function (error0, connection) {
+//   if (error0) {
+//     throw error0;
+//   }
+//   connection.createChannel(function (error1, channel) {
+//     if (error1) {
+//       throw error1;
+//     }
 
-    var queue = 'ondc';
+//     var queue = 'ondc';
 
-    channel.assertQueue(queue, {
-      durable: false
-    });
+//     channel.assertQueue(queue, {
+//       durable: false
+//     });
 
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+//     console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-    channel.consume(queue, function (msg) {
-      console.log(" [x] Received %s", msg.content.toString());
-      Received_msg_id = msg.content.toString()
-    }, {
-      noAck: true
-    });
-  });
-});
+//     channel.consume(queue, function (msg) {
+//       console.log(" [x] Received %s", msg.content.toString());
+//       Received_msg_id = msg.content.toString()
+//     }, {
+//       noAck: true
+//     });
+//   });
+// });
 var currentdate = new Date();
 var datetime = currentdate.getDate() + "-"
   + (currentdate.getMonth() + 1) + "-"
